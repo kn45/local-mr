ebf41c5ad74dfed103e13fe527eb9751c6e63855
kn45 <381404825@qq.com>
2018-04-19 14:27:11 +0800

    add partitioner

--- test_red.sh
+++ test_red.sh
@@ -6,5 +6,7 @@ done
 ./localmr \
 --input=./test_mr_input \
 --output=./test_mr_output \
---mapper="awk '{print NR}'" \
---nmap=2
+--mapper="awk '{print \$0}'" \
+--reducer="cat" \
+--nmap=2 \
+--nred=5
